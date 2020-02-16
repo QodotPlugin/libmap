17481644476503ba94f28d745ff69fe48da5ea3b
Josh Palmer <jpalmerwatkins@gmail.com>
2020-02-16 04:17:22 +0000

    Update DLL export defines

--- h/libmap.h
+++ h/libmap.h
@@ -3,6 +3,21 @@
 
 #include <stdbool.h>
 
-#define LIBMAP_API __declspec(dllexport)
+#if defined(_MSC_VER)
+    //  Microsoft 
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+    //  GCC
+    #define EXPORT __attribute__((visibility("default")))
+    #define IMPORT
+#else
+    //  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
+#define LIBMAP_API EXPORT
 
 #endif
